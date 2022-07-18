d08cb2cfdf74518c19a1602b79f7196e14ef276e
themarjanz <themarjanz@gmail.com>
2022-07-18 18:30:18 +0600

    first commit

---
+++ case-convert.js
@@ -0,0 +1,10 @@
+const anthem = 'Amar sonar bangla ami tumay valobasghi';
+const search = 'valo';
+
+const userInput = 'blACkPinK';
+const savedUserName = 'blackPink';
+// console.log(userInput.toLocaleLowerCase());
+if (userInput.toLocaleLowerCase() == savedUserName.toLocaleLowerCase()) {
+    console.log('user exits');
+
+}
